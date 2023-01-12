5c705aa4723630f1af6eee3e1c0e59e1927facc3
1freshie <hristian.todorov14@gmail.com>
2023-01-12 09:44:18 +0200

    added custom fontFamily, fontSize and colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,58 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "dm-serif": ["DM Serif Display", "serif"],
+      "avenir-book": ["Avenir-Book", "sans-serif"],
+    },
+    fontSize: {
+      "sm": [
+        "18px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "0.4px",
+          fontWeight: "400",
+        },
+      ],
+      "base": [
+        "20px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "0.6px",
+          fontWeight: "400",
+        },
+      ],
+      "lg": [
+        "30px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0.6px",
+          fontWeight: "400",
+        },
+      ],
+      "xl": [
+        "32px",
+        {
+          lineHeight: "40px",
+          letterSpacing: "0.6px",
+          fontWeight: "400",
+        },
+      ],
+      "2xl": [
+        "40px",
+        {
+          lineHeight: "48px",
+          letterSpacing: "0.8px",
+          fontWeight: "400",
+        },
+      ],
+    },
+    colors: {
+      "primary--pale-teal": "#7FC6AC",
+      "secondary--dark-cyan": "#066A76",
+      "secondary--light-pink": "EBE3E9",
+      "primary--black": "#161615",
+    },
     extend: {},
   },
   plugins: [],
